016e19b07c70ca887a866fa5c766503769421dc8
Charan-2009 <71555866+Charan-2009@users.noreply.github.com>
2020-11-23 20:13:34 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,54 @@
+class Block  {
+    constructor(x,y,width,height)  {
+        var options={
+            isStatic:false
+        }
+       
+        this.width = width
+        this.height = height
+        this.body = Bodies.rectangle(x,y,width,height,options)
+        
+        this.visibility = 255;
+        World.add(world,this.body)
+        
+    }
+    display()  {
+
+        if(this.body.speed<3){
+            // rectMode(CENTER)
+            // push();
+            // fill(171,247,247)
+            // rect(this.body.position.x,this.body.position.y,this.width,this.height)
+            // pop();
+            var angle = this.body.angle;
+            var pos = this.body.position;
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            fill(171,247,247)
+            rect(0,0,this.width, this.height);
+            pop();
+
+        }
+        else{
+            World.remove(world, this.body);
+            push();
+            translate(this.body.position.x, this.body.position.y);
+            this.visibility = this.visibility -5;
+            //Creating a disintegrating effect
+            tint(255,this.visibility);
+            pop();
+        }
+
+        
+
+    }
+
+    score(){
+        if(this.visibility<0 && this.visibility>-100){
+          score = score +1
+        }
+      } 
+    
+}
